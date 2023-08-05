3086aabbecf87efcd78effb1c1f96b541755acf3
Cho man shu <amandamac@ChodeMacBook-Pro.local>
2023-08-06 00:42:47 +0800

    init project
    init

---
+++ src/test/java/com/example/java_spring_boot/JavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

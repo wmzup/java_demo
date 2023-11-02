55a927fae9bd0c6e6769628427c36bd6a96bef3f
amanda.chou <amanda.cho@shoalter.com>
2023-11-02 18:20:52 +0800

    feat: add other properties

--- src/main/java/com/example/java_spring_boot/config/MailConfig.java
+++ src/main/java/com/example/java_spring_boot/config/MailConfig.java
@@ -4,9 +4,11 @@ import lombok.Data;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
+@PropertySource("classpath: mail.properties")
 public class MailConfig {
 
     @Value("${mail.host}")
--- src/main/java/com/example/java_spring_boot/controller/MailController.java
+++ src/main/java/com/example/java_spring_boot/controller/MailController.java
@@ -2,10 +2,12 @@ package com.example.java_spring_boot.controller;
 
 import com.example.java_spring_boot.dto.request.SendMailRequest;
 import com.example.java_spring_boot.service.MailService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +19,7 @@ public class MailController {
     private MailService mailService;
 
     @PostMapping
-    public ResponseEntity<Void> sendMail(SendMailRequest request) {
+    public ResponseEntity<Void> sendMail(@Valid @RequestBody SendMailRequest request) {
         mailService.sendMail(request);
         return ResponseEntity.noContent().build();
     }

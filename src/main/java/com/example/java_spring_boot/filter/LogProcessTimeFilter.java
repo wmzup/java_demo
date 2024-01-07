ab1253fc0a7b30c287f6a3fc5b2e6914f2bceb43
Amanda Chou <wmzup124@gmail.com>
2024-01-07 22:15:40 +0800

    feat: implement doFilterInternal

---
+++ src/main/java/com/example/java_spring_boot/filter/LogProcessTimeFilter.java
@@ -0,0 +1,22 @@
+package com.example.java_spring_boot.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class LogProcessTimeFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(request, response);
+        long processTime = System.currentTimeMillis() - startTime;
+
+        System.out.println(processTime + " ms");
+    }
+}

4bc1aa6b5d9f8df25864425c1d3c2db1421834d5
Amanda Chou <wmzup124@gmail.com>
2024-01-08 21:31:54 +0800

    feat: register log api filter

--- src/main/java/com/example/java_spring_boot/config/FilterConfig.java
+++ src/main/java/com/example/java_spring_boot/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.example.java_spring_boot.config;
 
+import com.example.java_spring_boot.filter.LogApiFilter;
 import com.example.java_spring_boot.filter.LogProcessTimeFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +10,23 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
+    public FilterRegistrationBean logApiFilter() {
+        FilterRegistrationBean<LogApiFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new LogApiFilter());
+        bean.addUrlPatterns("/*");
+        bean.setName("logApiFilter");
+        bean.setOrder(0);
+
+        return bean;
+    }
+
+    @Bean
     public FilterRegistrationBean logProcessTimeFilter() {
         FilterRegistrationBean<LogProcessTimeFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new LogProcessTimeFilter());
         bean.addUrlPatterns("/*");
         bean.setName("logProcessTimeFilter");
+        bean.setOrder(1);
 
         return bean;
     }
--- src/main/java/com/example/java_spring_boot/filter/LogApiFilter.java
+++ src/main/java/com/example/java_spring_boot/filter/LogApiFilter.java
@@ -4,19 +4,29 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class LogProcessTimeFilter extends OncePerRequestFilter {
+@Configuration
+public class LogApiFilter extends OncePerRequestFilter {
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        long startTime = System.currentTimeMillis();
         filterChain.doFilter(request, response);
-        long processTime = System.currentTimeMillis() - startTime;
 
-        System.out.println(processTime + " ms");
+        int httpStatus = response.getStatus();
+        String httpMethod = request.getMethod();
+        String uri = request.getRequestURI();
+        String params = request.getQueryString();
+
+        if (params != null) {
+            uri += "?" + params;
+        }
+
+        System.out.println(String.join(" ", String.valueOf(httpStatus), httpMethod, uri));
     }
 }

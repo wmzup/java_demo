64126c77dcf5723b65610a3bdf35b3156f0fb8a7
Amanda Chou <wmzup124@gmail.com>
2024-01-08 22:01:43 +0800

    feat: catch body by ContentCachingResponseWrapper

--- src/main/java/com/example/java_spring_boot/filter/LogApiFilter.java
+++ src/main/java/com/example/java_spring_boot/filter/LogApiFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 
@@ -16,8 +18,17 @@ public class LogApiFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        filterChain.doFilter(request, response);
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(requestWrapper, responseWrapper);
 
+        logAPI(request, response);
+        logBody(requestWrapper, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
+    }
+
+    private void logAPI(HttpServletRequest request, HttpServletResponse response) {
         int httpStatus = response.getStatus();
         String httpMethod = request.getMethod();
         String uri = request.getRequestURI();
@@ -28,5 +39,18 @@ public class LogApiFilter extends OncePerRequestFilter {
         }
 
         System.out.println(String.join(" ", String.valueOf(httpStatus), httpMethod, uri));
+    }
+
+    private void logBody(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        String requestBody = getContent(request.getContentAsByteArray());
+        System.out.println("request: " + requestBody);
+
+        String responseBody = getContent(response.getContentAsByteArray());
+        System.out.println("response: " + responseBody);
+    }
+
+    private String getContent(byte[] content) {
+        String body = new String(content);
+        return body.replaceAll("[\n\t]", body);
     }
 }
